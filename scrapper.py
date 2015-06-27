68e4147d306410d2b3d47fd9fee280f5a42e2851
Sajani <deepasaj@thoughtworks.com>
2015-06-27 21:35:17 +0530

    Parse all locations and store it as a key value pair

--- scrapper.py
+++ scrapper.py
@@ -2,7 +2,10 @@ from bottle import route, run
 import requests
 import code
 from pyquery import PyQuery
-from newspaper import Article 
+from newspaper import Article
+import csv 
+
+result = {}
 
 @route('/hello')
 def hello():
@@ -14,13 +17,21 @@ def crimes():
 	web_page = requests.get(base_url + '/city/chennai?cfmid=2000000')
 	parsed_content = PyQuery(web_page.text)
 	all_crimes = parsed_content('div.ct1stry h2 a')
+	pruneDataSet()
 	for crime in all_crimes:
-		code.interact(local=locals())
 		crime_url = base_url + crime.attrib["href"]
 		article = Article(crime_url)
 		article.download()
 		article.parse()
 		article.nlp()
 		keywords = article.keywords
+
+def pruneDataSet():
+	with open('chennai.csv', 'rt') as csvfile:
+		spamreader = csv.reader(csvfile, delimiter=',')
+		for row in spamreader:
+			result[row[0]] = row[1]
+		print(result)
+
 
 run(host='localhost', port= 8080, debug=True)
